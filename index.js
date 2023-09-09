41fbce12dd3884a01f63cf6101eac64e93983bea
Siarhei Iukou <siarhei_iukou@epam.com>
2023-09-09 23:28:19 +0300

    feature: [module_2] get process information and write to a log file

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
-const getRandomNumber = (min = 0, max = 100) => {
-  const randomNumber = Math.floor(Math.random() * (max - min) + min);
+const os = require('os');
+const fs = require('fs');
+const { exec } = require('child_process');
 
-  console.log(randomNumber);
+const WINDOWS_COMMAND = `powershell "Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + ' ' + $_.CPU + ' ' + $_.WorkingSet }"`;
+const OTHER_PLATFORM = 'ps -A -o %cpu,%mem,comm | sort -nr | head -n 1';
+
+const WINDOWS_PLATFORM = 'win32';
+let log = '';
+
+const getUnixTime = () => Math.round(new Date().getTime() / 1000);
+
+const startExecProcess = (command) => {
+  exec(command, (error, stdout, stderr) => {
+    console.clear();
+    if (stdout) {
+      console.log(`stderr: ${stdout}`);
+      log += `${getUnixTime()} : ${stdout}`;
+    }
+
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+    }
+
+    if (error !== null) {
+      console.log(`error: ${error}`);
+    }
+  });
 }
 
-getRandomNumber();
+const startWritingToLogFile = () => setInterval(() => {
+  fs.appendFile('activityMonitor.log', log, (error) => {
+    if (error) {
+      throw error;
+    };
+  });
+  log = '';
+}, 6000);
+
+const startProcess = () => {
+  if (os.platform() === WINDOWS_PLATFORM) {
+    setInterval(() => startExecProcess(WINDOWS_COMMAND), 1000);
+  } else {
+    setInterval(() => startExecProcess(OTHER_PLATFORM), 1000);
+  }
+
+  startWritingToLogFile();
+}
+
+startProcess();
